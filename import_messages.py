2c94dce41c3292044093ba4ae1d0ca9f7e3007e9
Lee Azzarello <lee@rockingtiger.com>
2024-09-25 15:54:39 -0700

    ignore and env

--- import_messages.py
+++ import_messages.py
@@ -7,22 +7,23 @@ load_dotenv()
 # channel_id = os.environ['CHANNEL_ID']
 token = os.environ['DISCORD_TOKEN']
 channel_id = 0
-username = "thebeautifulpuffin"
-limit = 100
+username = os.environ['DISCORD_USERNAME']
+schedule = os.environ['DISCORD_CHANNEL']
+limit = None 
 filename = "messages.txt"
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
         for channel in self.get_all_channels():
-            if channel.name == 'schedule-pacific':
+            if channel.name == schedule:
                 channel_id = channel.id
                 break
         channel = self.get_channel(channel_id)
         # https://discordpy.readthedocs.io/en/stable/api.html#discord.TextChannel.history
         # channel history is only about 500K and saves in a few seconds, nbd!
         with open(filename, 'w') as file:
-            messages = [message async for message in channel.history(limit=None, oldest_first=True)]
+            messages = [message async for message in channel.history(limit=limit, oldest_first=True)]
             for message in messages:
                 if message.author.name == username:
                     file.write(f'{message.created_at} : {message.content}\n')
